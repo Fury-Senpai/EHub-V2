9ed8a438d162a955ad808b4ca007c71fa289b0dd
Fury-Senpai <harbola03nitish@gmail.com>
2026-02-08 22:44:43 +0530

    removed base in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
-  base: process.env.VITE_API_URL || "/tree/main/client"
+  // base: process.env.VITE_API_URL || "/tree/main/client"
 })
